98a38fc115c8fc8ec7f681799db4cc7d15b674db
mohiuddinsizan <mohiuddinsizan13@gmail.com>
2026-03-19 02:04:36 +0600

    v3

--- books-frontend/src/api/axios.js
+++ books-frontend/src/api/axios.js
@@ -2,14 +2,16 @@ import axios from "axios";
 import { getStoredAuth } from "../utils/storage";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
   const auth = getStoredAuth();
+
   if (auth?.token) {
     config.headers.Authorization = `Bearer ${auth.token}`;
   }
+
   return config;
 });
 
